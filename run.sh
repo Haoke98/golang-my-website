55cf17916aca602a95b0d0514a4d00006eac8442
Sadam <1903249375@qq.com>
2021-04-06 00:40:21 +0800

    优化更新换代了自动化启动脚本。

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-nohup ./linuxBuild_linux > linuxBuild_linux.log 2>&1 &
+RUNFILE="./linuxBuild_linux"
+PID=$(pgrep -f ./linuxBuild_linux)
+echo $PID
+$(kill ${PID})
+echo "killing Model:${RUNFILE} PID:${PID} now ......"
+nohup ${RUNFILE} > ${RUNFILE}.log 2>&1 &
