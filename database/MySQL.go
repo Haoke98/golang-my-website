47447f1e74093837da39957a461ad0c75cd40ad0
Sadam <1903249375@qq.com>
2021-04-17 18:07:37 +0800

    修改了MYSQL数据库链接信息，把用户名，密码，链接地址，端口等铭感信息都放在DB.go文件里于配置文件隔离开来，并设置gitignore文件不要DB.go文件提交到git上。

--- database/MySQL.go
+++ database/MySQL.go
@@ -2,11 +2,18 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 )
 
 func GetDB() *sql.DB {
-	db, err := sql.Open("mysql", "root:qwer1234@tcp(139.155.30.83:3306)/izbasar?charset=utf8")
+	IpAddr := MYSQL_HOST
+	port := MYSQL_PORT
+	databaseName := MYSQL_DATABASE
+	username := MYSQL_USERNAME
+	password := MYSQL_PASSWORD
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8", username, password, IpAddr, port, databaseName))
 	if err != nil {
 		log.Print("An error has been occurred when connecting to the database server:", err)
 		return db
